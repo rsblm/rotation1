c8a8b3c97177c624bbc4357c03dfdd1f23a1e685
Rosa Stolper <immd0754@bioch.ox.ac.uk>
2017-12-19 15:43:38 +0000

    added using input from MinPlus_ann instead of names of plus/minus strand

--- countoverlap_fun.R
+++ countoverlap_fun.R
@@ -1,6 +1,5 @@
-##### Use CountOverlaps to count overlap between bins and bigwig files
-
-overlapcount <- function(bin_grange, filesbw, bin_matrix) {
+overlapcount <- function(bin_grange, filesbw, bin_matrix, MinPlus_ann) {
+##### Use CountOverlaps from GenomicRanges package to count overlap between bins and bigwig files and imports bigwig files with rtracklayer
 
   suppressPackageStartupMessages(require("GenomicRanges"))
   suppressPackageStartupMessages(require("rtracklayer"))
@@ -9,21 +8,18 @@ overlapcount <- function(bin_grange, filesbw, bin_matrix) {
   binned_matrix <- matrix(NA, nrow =  length(bin_grange), ncol = length(filesbw))
 
   colnames(binned_matrix) <- names(filesbw)
-  rownames(binned_matrix) <- bin_matrix[,2] ### try to make it Bin + bin number
-
 
   for (i in seq_len(length(filesbw))) {
-  
-  
+
     bigimp <- import.bw(filesbw[i])
   
-    if (any(grep("plus|posstrand|unipos", filesbw[i]))) {
+    if (any(grep(MinPlus_ann[[1]], filesbw[i]))) {
       strand(bigimp) <- "+"
     } else {
-      if (any(grep("minus|negstrand|unimin", filesbw[i]))) {
+      if (any(grep(MinPlus_ann[[2]], filesbw[i]))) {
         strand(bigimp) <- "-"
       }else{
-        print("no strand specified")
+        print("no strand specification found")
       }
     }
   
