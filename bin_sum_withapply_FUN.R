4612365b81e3ddc1a0ed28dc857cb1c0b48abea7
Rosa Stolper <immd0754@bioch.ox.ac.uk>
2017-12-19 11:04:40 +0000

    Made a shorter function with using by() with indices that is supposed to be faster than the nested forloop approach

--- bin_sum_withapply_FUN.R
+++ bin_sum_withapply_FUN.R
@@ -1,65 +1,7 @@
-bin_sum <- function(binned_matrix, i, j) {
-  sum(binned_matrix[which(binned_matrix[,1]==i),(j+1)])
+bin_sum <- function(binned_matrix, stri) {
+# Takes the sum of subset of rows(by bin number) in the columns (samples)
+  #Expects a matrix for binned_matrix and a list (or integer?) with binnumbers. nrow of binned_matrix must be length(stri)
+  dm3 <- by(binned_matrix, INDICES = stri, colSums)
+  return(as.data.frame(do.call(rbind,dm3)))
 }
 
-
-m3 <- mapply(binned_matrix, i=(1:numberbin), j=(1:length(filesbw)), FUN=bin_sum)
-
-m2 <- mapply(binned_matrix, FUN=bin_sum)
-
-
-m2 <- matrix(NA, nrow=numberbin, ncol = length(filesbw))
-
-
-dummy <- function(x, y) paste(x, y)
-sapply(1:4, function(i) sapply(1:4, function(j) dummy(nfl_teams[i], years[j])))
-
-
-m2 <- sapply(1:numberbin, function(i) sapply(1:length(filesbw), function(j) bin_sum(binned_matrix[i,j])))
-
-############################
-
-
-bin_sum <- function(binned_matrix, i, j, stri) {
-   sum(binned_matrix[which(stri==i),(j)])
-
-  }
-
-
-### This gives the error incorrect number of dimensions
-
-bin_sum <- function(binned_matrix, m3, i, j, stri) {
-  m3[i,j] <- sum(binned_matrix[which(stri==i),(j)])
-  return(m3)
-}
-
-
-
-test_binned_matrix <- binned_matrix[,2:13]
-stri <- binned_matrix[,1]
-
-
-m4 <- apply(m2, MARGIN=c(1,2), FUN=bin_sum, test_binned_matrix,i=(1:300), j=(1:12), stri=stri)
-
-sum(test_binned_matrix[which(stri==1),(1)])
-
-#### Use by?
-
-bin_sum <- function(binned_matrix, i, j, stri) {
- sum(binned_matrix[which(stri==i),(j)])
- 
-}
-
-dm3 <- by(test_binned_matrix, INDICES = stri, colSums)
-as.data.frame(do.call(cbind,dm3, check.names))
-return(as.data.frame(do.call(cbind,combined)))
-
-
-dm4 <- as.matrix(unlist(dm3))
-
-dm2 <- as.data.frame(cbind(dm3))
-dm5 <- unlist(dm2, recursive=FALSE)
-
-dm5 <- as.data.frame(do.call(rbind,dm3))
-
-c <- by(t(test_binned_matrix))
