afd2520aa5713bff089653e511ea96ff9410e662
Rosa Stolper <immd0754@bioch.ox.ac.uk>
2017-12-19 15:41:29 +0000

    Comment on what to do

--- combstrand_fun.R
+++ combstrand_fun.R
@@ -2,5 +2,6 @@ comstrand <- function(metagene_table) {
 #Combines columns with the same names, so it's meant to be combine plus and minus strand
   #Expects metagene_table as a dataframe with column names
 combined <- by(t(metagene_table), INDICES = rownames(t(metagene_table)), FUN=colSums)
+  #Needs an if stop loop if no cols with the same name
 return(as.data.frame(do.call(cbind,combined)))
 }
