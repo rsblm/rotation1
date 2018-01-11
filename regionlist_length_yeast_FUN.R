4a21efed9aed25e8fa98faf0b746872877b9d807
Rosa Stolper <rosastolper@hotmail.com>
2018-01-11 11:21:39 +0000

    Renamed newest version of regionlist function for yeast select genes of specific length

---
+++ regionlist_length_yeast_FUN.R
@@ -0,0 +1,33 @@
+regionlist <- function (plus, minus, pos_strand="//biochstore4.bioch.ox.ac.uk/Mellor/Rosa/pos_strand_annotations.bed", neg_strand="//biochstore4.bioch.ox.ac.uk/Mellor/Rosa/neg_strand_annotations.bed") {
+#### List of regions of interest (for instance promoter proximal) based on reference genes/regions/genome
+# For Yeast: Standard is genes according to Steinmetz lab Pechano et al. 2013
+# This is a newer version that selects genes of certain lengths (currently >1000, <4000)
+# Also output is now both for genes_plus and genes_minus combined as seperate 
+  
+  
+  
+  genes_plus <-read.table(pos_strand)
+  genes_minus <- read.table(neg_strand)
+
+#Select genes of length 1000<gene<4000    
+  promoter_regionPlus <- genes_plus[(genes_plus$V3-genes_plus$V2)>1000,]
+  promoter_regionPlus <- promoter_regionPlus[(promoter_regionPlus$V3-promoter_regionPlus$V2)<4000,]
+  promoter_regionPlus1 <- promoter_regionPlus
+  promoter_regionPlus[,3] <- promoter_regionPlus1[,2]+plus
+  promoter_regionPlus[,2] <- promoter_regionPlus1[,2]-minus
+  
+  promoter_regionMinus <- genes_minus[(genes_minus$V3-genes_minus$V2)>1000,]
+  promoter_regionMinus1 <- promoter_regionMinus[(promoter_regionMinus$V3-promoter_regionMinus$V2)<4000,]
+  promoter_regionMinus1 <- promoter_regionMinus
+  promoter_regionMinus[,3] <- promoter_regionMinus1[,3]+minus
+  promoter_regionMinus[,2] <- promoter_regionMinus1[,3]-plus
+  
+  promoter_region <- rbind(promoter_regionMinus, promoter_regionPlus) ### don't change the order of minus/plus region without changing the renaming of the rownames of the minus strand just after the CountOverlap bit (chunk 5)
+  
+  res <- list(promoter_region=promoter_region, genes_plus=promoter_regionPlus, genes_minus=promoter_regionMinus)
+  
+  return(res)
+}
+
+
+
