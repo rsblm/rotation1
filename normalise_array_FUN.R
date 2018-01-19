b47e5a01130e5dc162695d1a6e76c7b48f0b2188
Rosa Stolper <rosastolper@hotmail.com>
2018-01-19 17:06:43 +0000

    Revert some of this becasue removing 0's not necessary anymore

--- normalise_array_FUN.R
+++ normalise_array_FUN.R
@@ -3,8 +3,12 @@ normalise_array <- function(overview, filesbw) {
     f <- apply(overview_rev, c(1,3) ,FUN = sum)
     
     for (i in 1:(length(filesbw)/2)) {
-    overview[,,i] <- t(t(overview_rev[,,i])/f[,i])
-    }
+     f[which(f[,i]==0),i] <- 1 ### this is a bit shady since it assumes if sum=0 than max=0 so if it's /1 or not it's always 0
+        overview[,,i] <- overview_rev[,,i]/f[,i]
+      
+        }
     
 return(overview)
 }
+
+
