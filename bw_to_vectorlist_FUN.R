2a7f2223ddea9ecd9afdace45f66efa4f02b495c
Rosa Stolper <rosastolper@hotmail.com>
2018-01-19 17:03:26 +0000

    Made the chromosome length not only depended on max position bw file but also on max position gene files

--- bw_to_vectorlist_FUN.R
+++ bw_to_vectorlist_FUN.R
@@ -1,9 +1,15 @@
-bw_to_vectorlist <- function(bwfile) {
+bw_to_vectorlist <- function(bwfile, genes_plus, genes_minus) {
 
 b <- as.data.frame(bwfile)
     
 f <- as.vector(by(b$end, INDICES = b$seqnames, max)) # with chrM, used later to compare to sum(b$width)
 l <- f[!levels(b$seqnames)=="chrM"] # without chrM, used to make list with chromosomes
+k <- as.vector(by(genes_minus$V3, INDICES = genes_minus$V1, max))
+n <- as.vector(by(genes_plus$V3, INDICES = genes_plus$V1, max))
+q <- rbind(l,n,k)
+l <- apply(q, 2, max)
+
+read_dens <- sum(b$score)/sum(q[1,])
 
 t <- lapply(l, function(x) vector("numeric", x))
 chr_names <- levels(b$seqnames)
@@ -22,9 +28,12 @@ if (max(b$width)==1) {
     t[[as.character(names(t)[[i]])]] <- rep(b[(b$seqnames==as.character(names(t)[[i]])),]$score, times=((b[(b$seqnames==as.character(names(t)[[i]])),]$end-b[(b$seqnames==as.character(names(t)[[i]])),]$start)+1)) # works for type test_bw2 (continuous, same length as max value, variable width)
   }
   } else {
-    print("For now this doesn't work yet with this bw file, because it neither has continues values nor only the values with width 1. I'm sure you can solve it if you look in the second approach (after else if) an combine it with the first approach")
+    print("For now this doesn't work yet with this bw file, because it neither has continues values nor only the values with width 1. I'm sure you can solve it if you look in the second approach (after else if) and combine it with the first approach")
   }
-  
+ 
+
+t <- list(t=t, read_dens=read_dens)
+ 
 return(t)
 
 }
