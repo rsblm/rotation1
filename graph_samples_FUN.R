e59939b36a1e215ff0e0890f0a9cd54561be47bd
Rosa Stolper <rosastolper@hotmail.com>
2018-01-19 17:07:49 +0000

    Same old

--- graph_samples_FUN.R
+++ graph_samples_FUN.R
@@ -1,4 +1,4 @@
-graph_samples <- function(combined, minus, plus, numberbin, name){
+graph_samples <- function(combined, minus, plus, name, filesbw){
 #Function that makes a linegraph for every sample in the same graph, detects number of samples etc
   #Expects a dataframe with columns for each sample and row being bins
   
@@ -6,12 +6,11 @@ graph_samples <- function(combined, minus, plus, numberbin, name){
   suppressPackageStartupMessages(require(reshape2)) # is necessary for melt
   suppressPackageStartupMessages(require(ggthemes))
   
-  combined2 <- cbind(rownames(combined) <- round(seq(-minus, plus, (plus-(-minus))/(numberbin-1))), combined, row.names=NULL)
-  colnames(combined2) <- c("bp", colnames(combined))
-  mcom <- melt(combined2, id="bp", measure=colnames(combined))
-  mcom2 <- mcom[6000:6600,]
+  combined2 <- cbind(seq(-minus, plus-1, 1), as.data.frame(combined), row.names=NULL)
+  colnames(combined2) <- c("bp", unique(names(filesbw)))
+  cmcom <- melt(combined2, id="bp", measure=unique(names(filesbw)))
   
-  lp <- ggplot(mcom, aes(bp, value, colour = variable)) + geom_line()
+  lp <- ggplot(cmcom, aes(bp, value, colour = variable)) + geom_line()
   lp2 <- lp + ggtitle(paste("Metageneplot from", name, "experiments", sep = " ")) + theme(plot.background = element_rect(fil="grey97"), panel.grid.major= element_line(colour = "grey80", size=0.001), panel.grid.minor= element_line(colour = "grey80", size=0.001), panel.background = element_rect(fill = "grey90"), legend.position = "right", legend.key=element_rect(fill=NA), legend.title=element_blank(), plot.title = element_text(size=15, face="bold", margin = margin(10, 0, 10, 0))) + scale_colour_tableau() + guides(colour = guide_legend(override.aes = list(size=1.5))) + xlab("Position to TSS (bp)") + ylab("Normalised Pol II occupancy")
 
 
